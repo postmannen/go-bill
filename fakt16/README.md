32ed575bde6d83aecdd5eebfc6b8e307753915b9
Bjørn Tore <bt@Macintosh.local>
2017-12-27 09:43:19 +0100

    Split the main package into main.go, web.go, and db.go.     Created README.md

--- fakt16/README.md
+++ fakt16/README.md
@@ -1,4 +1,3 @@
-/*
 2: Tested person struct, and string to int conversion for person ID.
     Iterate the slice of struct
 3: Added invoice nr. to personStruct
@@ -57,53 +56,3 @@ Ideas:
 	 between different devices without getting a conflict.
 	 Sorting can be done on a dummy index value that don't have to be unique
 
-
-
-*/
-package main
-
-import (
-	"database/sql"
-	"html/template"
-	"net/http"
-
-	_ "github.com/mattn/go-sqlite3"
-)
-
-//User is used for all customers and users
-type User struct { //some
-	Number         int
-	FirstName      string
-	LastName       string
-	Mail           string
-	Address        string
-	PostNrAndPlace string
-	PhoneNr        string
-	OrgNr          string
-	CountryID      string
-}
-
-var pDB *sql.DB                        //The pointer to use with the Database
-var tmpl map[string]*template.Template //map to hold all templates
-var indexNR int                        //to store the index nr. in slice where chosen person is stored
-
-func init() {
-	//initate the templates
-	tmpl = make(map[string]*template.Template)
-	tmpl["init.html"] = template.Must(template.ParseFiles("templates.html"))
-}
-
-func main() {
-	//create DB and store pointer in pDB
-	pDB = createDB()
-	defer pDB.Close()
-
-	http.HandleFunc("/sp", showUsersWeb)
-	http.HandleFunc("/ap", addUsersWeb)
-	http.HandleFunc("/mp", modifyUsersWeb)
-	http.HandleFunc("/", mainPage)
-	http.HandleFunc("/du", deleteUserWeb)
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.ListenAndServe(":7000", nil)
-
-}
---
+++ fakt16/db.go
@@ -0,0 +1,195 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+/****************************
+*	DATABASE FUNCTIONS		*
+*							*
+****************************/
+
+//Query the database for all users, and return a slice of struct with all users
+func queryDBForAllUserInfo(pDB *sql.DB) []User {
+	//get total rows in database
+	num, countLines := queryDBForLastCustomerUID(pDB)
+	p := []User{}
+	fmt.Println("queryDBForAllUserInfo : queryDBForAllUserInfo highestNR ER = ", num)
+	fmt.Println("queryDBForAllUserInfo : queryDBForAllUserInfo countlines = ", countLines)
+
+	for i := 1; i <= num; i++ {
+		//append the row to slice
+		pTemp := queryDBForSingleUserInfo(pDB, i)
+		if pTemp.Number != 0 {
+			p = append(p, queryDBForSingleUserInfo(pDB, i))
+		}
+	}
+	return p
+}
+
+//Query the database for the info of a single user. Takes user ID of type int as input, returns struct of single user
+func queryDBForSingleUserInfo(db *sql.DB, uid int) User {
+
+	rows, err := db.Query("select * from user where user_id=?", uid)
+	checkErr(err)
+
+	var pid int
+	//variables to store the rows.Scan below
+	var firstname, lastname, mail, address, postnrandplace, phonenr, orgnr, countryID string
+	//Next prepares the next result row for reading with the Scan method. It returns true on success,
+	//or false if there is no next result row or an error happened while preparing it.
+	//Err should be consulted to distinguish between the two cases.
+	for rows.Next() {
+		//Scan copies the columns in the current row into the values pointed at by dest.
+		//The number of values in dest must be the same as the number of columns in Rows of database.
+		rows.Scan(&pid, &firstname, &lastname, &mail, &address, &postnrandplace, &phonenr, &orgnr, &countryID)
+
+	}
+	m := User{}
+	m.Number = pid
+	m.FirstName = firstname
+	m.LastName = lastname
+	m.Mail = mail
+	m.Address = address
+	m.PostNrAndPlace = postnrandplace
+	m.PhoneNr = phonenr
+	m.OrgNr = orgnr
+	m.CountryID = countryID
+
+	defer rows.Close()
+	return m
+}
+
+//input *sql.DB and returns the highest uid number, and line count of rows in DB
+func queryDBForLastCustomerUID(db *sql.DB) (int, int) {
+	rows, err := db.Query("select user_id from user")
+	checkErr(err)
+	defer rows.Close()
+	//Prepare the slice to store numbers read from DB
+	var num []int
+	for rows.Next() {
+		var pid int
+		//The number of values below must be the same amount
+		//as the number of rows in the DB
+		err := rows.Scan(&pid) //puts data into the address of the variable
+		checkErr(err)
+		num = append(num, pid)
+	}
+
+	highestNr := 0
+	countLines := 0
+	//iterate the slice, and find the highest number, and number of lines.
+	for i := range num {
+		if highestNr < num[i] {
+			highestNr = num[i]
+			log.Println("queryDBForLastCustomerUID : highestNr = ", highestNr)
+			countLines++
+		}
+	}
+	log.Println("queryDBForLastCustomerUID: highestNr = ", highestNr)
+	log.Println("queryDBForLastCustomerUID: countLines = ", countLines)
+	return highestNr, countLines
+}
+
+//Update user in Database, takes pointer to db and type User struct as input
+func updateUserInDB(db *sql.DB, u User) {
+	tx, err := db.Begin()
+	checkErr(err)
+
+	log.Println("The org nr. sendt to updateUserDB function = ", u.OrgNr)
+	stmt, err := tx.Prepare("UPDATE user SET user_id=?,first_name=?,last_name=?,mail=?,address=?,post_nr_place=?,phone_nr=?,org_nr=?,country_id=? WHERE user_id=?")
+	checkErr(err)
+	defer stmt.Close()
+	log.Println("updateUserInDB : Number in updateUserInDB function = ", u.Number)
+	log.Println("************", u.Number, u.FirstName, u.LastName, u.Mail, u.Address, u.PostNrAndPlace, u.PhoneNr, u.OrgNr, u.CountryID, u.Number, "*************")
+	//number is passed an extra time at the end of DB statement to fill the variable for the Query, which is done by number of user
+	_, err = stmt.Exec(u.Number, u.FirstName, u.LastName, u.Mail, u.Address, u.PostNrAndPlace, u.PhoneNr, u.OrgNr, u.CountryID, u.Number)
+
+	tx.Commit()
+	checkErr(err)
+
+}
+
+//Adds user to Database. takes pointer to DB, and type User struct as input
+func addUserToDB(db *sql.DB, u User) {
+	//start db session
+	tx, err := db.Begin()
+	checkErr(err)
+
+	//create statement to insert values to DB
+	stmt, err := tx.Prepare("insert into user(user_id,first_name,last_name,mail,address,post_nr_place,phone_nr,org_nr,country_id) values(?,?,?,?,?,?,?,?,?)")
+	checkErr(err)
+	//At the end of function close the DB
+	defer stmt.Close()
+
+	//execute the statement on the DB
+	_, err = stmt.Exec(u.Number, u.FirstName, u.LastName, u.Mail, u.Address, u.PostNrAndPlace, u.PhoneNr, u.OrgNr, u.CountryID)
+	//commit to DB
+	tx.Commit()
+	checkErr(err)
+
+}
+
+//creates the database
+func createDB() *sql.DB {
+	//1. Open connection
+
+	db, err := sql.Open("sqlite3", "./fakt.db") //return types = *DB, error
+	checkErr(err)
+	//2. fail-fast if can't connect to DB
+	checkErr(db.Ping())
+	//3. create table
+	_, err = db.Exec(`create table if not exists user (
+						pid integer PRIMARY KEY, 
+						firstname string not null,
+						lastname string,
+						mail string,
+						address string,
+						postnrandplace string,
+						phonenr string,
+						orgnr string)
+						;`)
+	checkErr(err)
+
+	/*_, err = db.Exec(`CREATE TABLE IF NOT EXISTS bills (
+						pid integer PRIMARY KEY,
+						billNR string not null,
+						service string not null,
+						amount string not null,
+						priceExVat integer not null,
+						discount integer,
+						priceIncVat integer,
+						totalSumExVat integer,
+						totalSumIncVat integer)
+	`)
+	checkErr(err)*/
+
+	return db
+}
+
+//if error !=nil, print error message to web page
+func checkErr(err error, args ...string) {
+	if err != nil {
+		log.Printf("ERROR : %q: %s\n", err, args)
+	}
+}
+
+//Delete a row in DB, takes pointer to db, and index number uid which corresponds to column 1 in DB for input
+func deleteUserInDB(db *sql.DB, number int) {
+	tx, err := db.Begin()
+	checkErr(err)
+	log.Println("deleteUserInDB: The index number of the person to delete is = ", number)
+
+	//Make the sql statement to execute
+	stmt, err := tx.Prepare("DELETE FROM user WHERE user_id=?")
+	checkErr(err)
+
+	defer stmt.Close()
+	//prepare the statement with a value for the "?"
+	_, err = stmt.Exec(number)
+	tx.Commit()
+	checkErr(err)
+
+}
